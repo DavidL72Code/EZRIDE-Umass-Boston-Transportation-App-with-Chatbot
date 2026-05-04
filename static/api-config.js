e1b249da0521870d7dd7589082fc4951ad8803c6
DavidL72Code <davidducle2@gmail.com>
2026-05-04 01:41:26 -0400

    Point API to HF Space backend

--- static/api-config.js
+++ static/api-config.js
@@ -1,3 +1,3 @@
 // Set this to your Render backend URL after deploying.
 // Leave empty string "" to use relative URLs (local development).
-window.API_BASE = "https://ezride-umass-boston-transportation-app.onrender.com";
+window.API_BASE = "https://davidl72code-ezride-umass.hf.space";
